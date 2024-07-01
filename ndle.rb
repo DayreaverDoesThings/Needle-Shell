bbb4e4e10673d84cfcc2eb8ba5cc478cc8a03d62
Dayreaver <warducksoren@gmail.com>
2024-07-01 13:20:19 -0500

    Update ndle.rb

--- ndle.rb
+++ ndle.rb
@@ -1,4 +1,4 @@
-puts "Needle v1.0.5"
+puts "Needle v1.0"
 puts "Needle is a project made by DayreaverDoesThings that can do several system operations. Yes, i know, you can just type in the console, but this is my first project!"
 puts "This program requires Arch Linux!"
 puts "Avalible commands: ls, pwd, sudo pacman -Syu"
