69a80bd04127f0c2a390d10aaa4d01aaeaf26af3
Dayreaver <warducksoren@gmail.com>
2024-07-01 14:41:55 -0500

    Update ndle.rb

--- ndle.rb
+++ ndle.rb
@@ -2,7 +2,7 @@
 puts "Needle v1.0.5"
 puts "Needle is a project made by DayreaverDoesThings that can do several system operations. Yes, i know, you can just type in the console, but this is my first project!"
 puts "This program requires Arch Linux!"
-puts "Avalible commands: ls, pwd, sudo pacman -Syu, exit"
+puts "Avalible commands: ls, pwd, pacman, exit"
 
 loop do
     # ask user for a prompt
@@ -11,14 +11,14 @@ loop do
 
     case prompt
     # execute "ls" if prompt = "ls"
-    when 'ls'
-        system("ls")
+    when /^ls/
+        system(prompt)
     # execute "pwd" if prompt = "pwd"
-    when 'pwd'
-        system("pwd")
-    # execute "sudo pacman -Syu" if prompt = "sudo pacman -Syu"
-    when 'sudo pacman -Syu'
-        system("sudo pacman -Syu")
+    when /^pwd/
+        system(prompt)
+    # execute "pacman" if prompt = "sudo pacman -Syu"
+    when /pacman/
+        system(prompt)
     when 'exit'
         puts("exiting needle...")
         exit
