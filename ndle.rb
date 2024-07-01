cc3a3c396d81da30abac4f8b47eb22789edb5b51
Dayreaver <warducksoren@gmail.com>
2024-07-01 13:17:09 -0500

    Update ndle.rb

--- ndle.rb
+++ ndle.rb
@@ -1,4 +1,4 @@
-puts "Needle v1.0"
+puts "Needle v1.0.5"
 puts "Needle is a project made by DayreaverDoesThings that can do several system operations. Yes, i know, you can just type in the console, but this is my first project!"
 puts "This program requires Arch Linux!"
 puts "Avalible commands: ls, pwd, sudo pacman -Syu"
@@ -13,4 +13,7 @@ if prompt == "pwd"
 end
 if prompt == "sudo pacman -Syu"
     system("sudo pacman -Syu")
+end
+if prompt != "ls" || prompt != "pwd" || prompt != "sudo pacman -Syu"
+    puts "invalid prompt, exiting..."
 end
