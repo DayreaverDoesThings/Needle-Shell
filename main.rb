b0109633c3916a5103a066930e4a8e29d893e8e2
Dayreaver <warducksoren@gmail.com>
2024-07-01 13:05:35 -0500

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,12 +1,18 @@
 puts "Needle v1.0"
-puts "Needle is a project made by DayreaverDoesThings that can do several system operations. Yes, i know, you can just type in the console, but this is my first project!)
+puts "Needle is a project made by DayreaverDoesThings that can do several system operations. Yes, i know, you can just type in the console, but this is my first project!"
+puts "This program requires Arch Linux!"
+puts "Avalible commands: ls, pwd, sudo pacman -Syu"
 
-def prompt(*args)
-    print(*args)
-    gets.chomp
+system("cd ~")
+
+prompt = gets.chomp
+
+if prompt == "ls"
+    system("ls")
 end
-
-cmdPrompt = prompt "cmd: "
-cmdPrompt
-
-
+if prompt == "pwd"
+    system("pwd")
+end
+if prompt == "sudo pacman -Syu"
+    system("sudo pacman -Syu")
+end
