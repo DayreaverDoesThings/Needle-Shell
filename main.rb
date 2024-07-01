295652b215f5e90bdaf1f07e6494df3cb59099f4
Dayreaver <warducksoren@gmail.com>
2024-07-01 12:39:27 -0500

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,2 +1,10 @@
 puts "Needle v1.0"
 puts "Needle is a project made by DayreaverDoesThings that can do several system operations. Yes, i know, you can just type in the console, but this is my first project!)
+
+def prompt(*args)
+    print(*args)
+    gets.chomp
+end
+
+name = prompt "cmd: "
+name
