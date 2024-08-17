17d2b03201e275f2af5be9fe78fae4beb0560846
Dayreaver <warducksoren@gmail.com>
2024-08-17 14:50:52 -0500

    Update ndle.rb

--- ndle.rb
+++ ndle.rb
@@ -1,14 +1,23 @@
-# welcome message
-# please credit when using this please
+require 'logger'
+
+# initialize logger
+logfile = 'session.log'
+logger = Logger.new(logfile)
+
+# log session start
+logger.info("Session started at #{Time.now}")
+
+# display welcome message with details about the needle shell project
 if RUBY_PLATFORM =~ /linux/
-  puts "needle v1.2"
+    puts "needle v1.2"
+    logger.info("Needle shell v1.2 started")
     puts "needle is a command shell project created by dayreaverdoesthings. it is designed to execute several system-level operations in a streamlined manner."
     puts "this shell is specifically made for arch linux!"
     puts "the following commands are available for use:"
     puts "  - ls: list the contents of the current directory"
     puts "  - pwd: print the current working directory"
     puts "  - pacman: manage packages with the arch linux package manager"
-    puts "  - cd: change the current directory"
+    puts "  - cd: change the current directory (this command has limited functionality)"
     puts "  - mkdir: create new directories"
     puts "  - cat: display the contents of a file"
     puts "  - exit: close the needle shell"
@@ -16,72 +25,82 @@ if RUBY_PLATFORM =~ /linux/
     puts "type 'help' to see this list of commands at any time during your session"
 
     loop do
-        # ask user for a prompt
+        # prompt the user to enter a command
         print "#{Dir.pwd}>"
         prompt = gets.chomp
+        logger.info("Command entered: #{prompt}")
     
         case prompt
-        # execute "ls" with error handling
         when /^ls/
-            puts "running \"ls...\""
+            puts "executing 'ls' to list directory contents..."
             begin
                 system(prompt)
+                logger.info("Executed 'ls' successfully")
             rescue Exception => e
-                puts "error running ls: #{e.message}"
+                puts "an error occurred while running ls: #{e.message}"
+                logger.error("Error executing 'ls': #{e.message}")
             end
-        # execute "pwd" with error handling
         when /^pwd/
-            puts "running \"pwd...\""
+            puts "executing 'pwd' to display the current directory..."
             begin
                 system(prompt)
+                logger.info("Executed 'pwd' successfully")
             rescue Exception => e
-                puts "error running pwd: #{e.message}"
+                puts "an error occurred while running pwd: #{e.message}"
+                logger.error("Error executing 'pwd': #{e.message}")
             end
-        # execute "pacman" with error handling
         when /pacman/
-            puts "running \"pacman...\""
+            puts "executing 'pacman' for package management..."
             begin
                 system(prompt)
+                logger.info("Executed 'pacman' successfully")
             rescue Exception => e
-                puts "error running pacman: #{e.message}"
+                puts "an error occurred while running pacman: #{e.message}"
+                logger.error("Error executing 'pacman': #{e.message}")
             end
-        # execute "cd" with error handling
         when /^cd\s+(.+)/
-            puts "running \"cd...\""
+            puts "attempting to change directory with 'cd'..."
             dir = $1.strip
             begin
                 Dir.chdir(dir)
-                puts "changed directory to #{Dir.pwd}"
+                puts "successfully changed directory to #{Dir.pwd}"
+                logger.info("Changed directory to #{Dir.pwd}")
             rescue Errno::ENOENT
-                puts "directory not found: #{dir}"
+                puts "error: directory not found - #{dir}"
+                logger.error("Directory not found: #{dir}")
             rescue Errno::EACCES
-                puts "permission denied: #{dir}"
+                puts "error: permission denied - #{dir}"
+                logger.error("Permission denied: #{dir}")
             rescue Exception => e
-                puts "error changing directory: #{e.message}"
+                puts "an error occurred while changing directory: #{e.message}"
+                logger.error("Error changing directory: #{e.message}")
             end
-        # execute "mkdir" with error handling
         when /^mkdir\s+(.+)/
-            puts "running \"mkdir\"..."
+            puts "executing 'mkdir' to create a new directory..."
             begin
                 system(prompt)
+                logger.info("Executed 'mkdir' successfully")
             rescue Exception => e
-                puts "error running mkdir: #{e.message}"
+                puts "an error occurred while running mkdir: #{e.message}"
+                logger.error("Error executing 'mkdir': #{e.message}")
             end
-        # execute "cat" with error handling
         when /cat/
-            puts "running \"cat...\""
+            puts "executing 'cat' to display file contents..."
             begin
                 system(prompt)
+                logger.info("Executed 'cat' successfully")
             rescue Exception => e
-                puts "error running cat: #{e.message}"
+                puts "an error occurred while running cat: #{e.message}"
+                logger.error("Error executing 'cat': #{e.message}")
             end
-        # exit the program
         when 'exit'
-            puts "exiting needle..."
+            puts "exiting the needle shell..."
+            logger.info("Session ended at #{Time.now}")
+            logger.close
             exit
-        # help
         when 'help'
             puts "displaying the list of available commands..."
+            logger.info("Displayed help")
             puts "  - ls: list the contents of the current directory"
             puts "  - pwd: print the current working directory"
             puts "  - pacman: manage packages with the arch linux package manager"
@@ -90,12 +109,14 @@ if RUBY_PLATFORM =~ /linux/
             puts "  - cat: display the contents of a file"
             puts "  - exit: close the needle shell"
             puts "  - help: display this list of commands again"
-        # invalid input
         else
             puts "invalid input. please enter a valid command or type 'help' to see the list of available commands."
+            logger.warn("Invalid command entered: #{prompt}")
         end
     end
 else
-    puts "this program requires linux, please try again on a linux system."
+    puts "this program requires a linux operating system. please try again on a linux system."
+    logger.error("Attempted to run on non-Linux system")
+    logger.close
     exit
 end
